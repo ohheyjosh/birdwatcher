a6cbe9430995d23de1e2e592522cd5f7c89f500b
Josh Barnett <oh@heyjo.sh>
2021-07-31 00:19:03 -0400

    Added initial tests

---
+++ src/__tests__/content.test.js
@@ -0,0 +1,24 @@
+import { expect, test } from "@jest/globals";
+import { usernames, findUsernames, applyTags } from "../content";
+
+beforeEach(() => {
+  // set up document body
+  document.body.innerHTML = `
+    <main>
+      <span>@testusername</span>
+    </main>
+  `;
+})
+
+test("find test username and add to set", () => {
+  findUsernames(document);
+  expect(usernames).toContain("@testusername");
+});
+
+test("add tag to username span", () => {
+  const tags = {
+    "@testusername": "test tag"
+  };
+  applyTags(document, { tags }); // applyTags() doesn't actually need the tags arg, i just wanted prettier to stfu
+  expect(document.querySelector("span[title='test tag']"));
+})
