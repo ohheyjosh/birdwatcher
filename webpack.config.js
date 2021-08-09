f88b7bd4f46847f99654a10bd4fc35b14832ad69
Josh Barnett <oh@heyjo.sh>
2021-08-08 20:00:26 -0400

    added babel-loader, prettier, + webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
